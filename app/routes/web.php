76501d1894698a6fa28a8df5206ac6e4cb247ba9
Mugabe Rodrick <rodrickmugabe14@gmail.com>
2026-06-18 13:39:34 +0300

    Laravel task pages: Home, About, Services, Contact

---
+++ app/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
---
+++ app/services.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Services</title>
+</head>
+<body>
+    <h1>Services Page</h1>
+</body>
+</html>
